2a928671962bec5a295fb630b3655ce8196b4f5e
WindDistinct <ao.nicolasr@gmail.com>
2024-11-23 21:03:28 -0500

    Implementación Servicios de Consultas y verificación de consumo en la app web

---
+++ LosGrisesWeb/Controllers/ConsultaController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Web;
+using System.Web.Mvc;
+using LosGrisesWeb.ProxyConsultas;
+
+namespace LosGrisesWeb.Controllers
+{
+    public class ConsultaController : Controller
+    {
+        ServicioConsultasClient consultasClient = new ServicioConsultasClient();
+        // GET: Consulta
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult LibrosAlquilados()
+        {
+            ViewBag.ObtenerLibrosMasAlquilados = consultasClient.ObtenerLibrosMasAlquilados(DateTime.Now.AddYears(-1),DateTime.Now,null).OrderBy(cantidad => cantidad.VecesAlquilado);
+            return View();
+        }
+
+        public ActionResult FiltrarLibrosAlquilados(FormCollection fc)
+        {
+            String fechaInicio = fc["FechaInicio"];
+            String fechaFin = fc["FechaFin"];
+            String libro = fc["Libro"];
+            ViewBag.ObtenerLibrosMasAlquilados = consultasClient.ObtenerLibrosMasAlquilados(Convert.ToDateTime(fechaInicio), Convert.ToDateTime(fechaFin), libro);
+
+            return View("LibrosAlquilados");
+        }
+
+        public ActionResult GenerosSolicitados()
+        {
+            ViewBag.ObtenerGenerosMasSolicitados = consultasClient.ObtenerGenerosMasSolicitados().OrderBy(genero => genero.Genero);
+            return View();
+        }
+
+        public ActionResult EditorialesAlquiladas()
+        {
+            ViewBag.EditorialesMasAlquiladas = consultasClient.ObtenerEditorialesMasAlquiladas(DateTime.Now.AddYears(-1), DateTime.Now).OrderBy(cantidad => cantidad.VecesAlquilada);
+            return View();
+        }
+
+        public ActionResult EditorialesXRentas(FormCollection fc)
+        {
+            String fechaInicio = fc["FechaInicio"];
+            String fechaFin = fc["FechaFin"];
+
+            ViewBag.EditorialesMasAlquiladas = consultasClient.ObtenerEditorialesMasAlquiladas(Convert.ToDateTime(fechaInicio),Convert.ToDateTime(fechaFin)).OrderBy(cantidad => cantidad.VecesAlquilada);
+
+            return View("EditorialesAlquiladas");
+        }
+
+        public ActionResult EjemplaresDisponibles()
+        {
+            ViewBag.EjemplaresDisponibles = consultasClient.ConsultarEjemplaresDisponibles(null);
+            return View();
+        }
+        public ActionResult BuscarLibro(FormCollection fc)
+        {
+            String libro = fc["Libro"];
+
+            ViewBag.EjemplaresDisponibles = consultasClient.ConsultarEjemplaresDisponibles(libro);
+
+            return View("EjemplaresDisponibles");
+        }
+
+        public ActionResult RentasXFechas()
+        {
+            ViewBag.RentasXFechas = consultasClient.ObtenerRentasPorFechas(DateTime.Now.AddYears(-1), DateTime.Now, null).OrderBy(fecha => fecha.rent_fec);
+            return View();
+        }
+        public ActionResult FiltrarRentas(FormCollection fc)
+        {
+            String fechaInicio = fc["FechaInicio"];
+            String fechaFin = fc["FechaFin"];
+            String cliente = fc["Cliente"];
+            ViewBag.RentasXFechas = consultasClient.ObtenerRentasPorFechas(Convert.ToDateTime(fechaInicio),Convert.ToDateTime(fechaFin),cliente).OrderBy(fecha => fecha.rent_fec);
+
+            return View("RentasXFechas");
+        }
+    }
+}
