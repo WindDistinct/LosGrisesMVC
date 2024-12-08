c4109af5d7687400d8923ada9b4417b90a60d6e3
Francisco Alberca <francisco.albercac@gmail.com>
2024-12-08 14:45:02 -0500

    Add login and launch route

---
+++ LosGrisesWeb/Controllers/LoginController.cs
@@ -0,0 +1,38 @@
+﻿using LosGrisesWeb.ProxyLogin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LosGrisesWeb.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        ServicioLoginClient servicioLogin = new ServicioLoginClient();
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string username, string password)
+        {
+            var login = servicioLogin.AutenticarUsuario(username, password);
+
+            if(login == true)
+            {
+                Session["Username"] = username;
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Usuario o contraseña incorrectos.";
+                return View();
+            }
+        }
+    }
+}
